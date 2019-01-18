3706e8297f72c27f2625be139678c59782456afe
Jean-Pierre Schober <jps@me.com>
2019-01-18 18:16:33 +0100

    Fixed test.

--- test/tesseract.js
+++ test/tesseract.js
@@ -1,15 +1,16 @@
 'use strict';
 
-var tesseract = require('../lib/tesseract');
-var should = require('should');
+const tesseract = require('../lib/tesseract');
+const should = require('should');
 
 
 describe('process', function(){
   it('should return the string "node-tesseract"', function(done){
 
-    var testImage = __dirname + '/test.png';
+    const testImage = __dirname + '/test.png';
 
     tesseract.process(testImage, function(err, text) {
+      should.not.exists(err);
       text.trim().should.equal('node-tesseract');
       done();
     });
